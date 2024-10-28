eccbd4784a8489794d4830591a9fe33f1262d778
Ritik Jangir <ritik.jangir@zucolservices.com>
2024-10-28 17:36:12 +0530

    chore: vite config - changed for dev

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   assetsInclude: ['**/*.gltf'],  // Add this line
+  server: {
+    host: true, // or '0.0.0.0'
+    port: 5137
+  },
 })

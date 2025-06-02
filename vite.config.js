a1b2fd288c92dfd40f18305906ac7e533018d517
Ritik Jangir <jangir.ritik06@gmail.com>
2025-06-02 23:54:03 +0530

    experimental: codebase - code for deploying on vercel (rollback to previous commit if needed)

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,27 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
   build: {
     sourcemap: false,
+    outDir: "dist",
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          "three-vendor": [
+            "three",
+            "@react-three/fiber",
+            "@react-three/drei",
+            "@react-three/postprocessing",
+          ],
+        },
+      },
+    },
   },
-  assetsInclude: ['**/*.gltf'],
+  assetsInclude: ["**/*.gltf"],
   server: {
+    port: process.env.PORT || 5137,
     host: true,
-    port: 5137
   },
-})
+});
